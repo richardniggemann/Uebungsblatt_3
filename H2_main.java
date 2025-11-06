85aeeb4541694f1fe0fdb7cfbefe8ab97037fac4
Richard Niggemann <rniggema@smail.uni-koeln.de>
2025-11-06 22:38:57 +0100

    solve homework 2

---
+++ H2_main.java
@@ -0,0 +1,14 @@
+public class H2_main {
+
+    public static void main(String[] args) {
+        int jahr = 2000;
+        boolean schalt = false;
+
+        schalt = isLeapYear(jahr);
+        System.out.println(jahr + " ist " + (schalt ? "" : "k") + "ein Schaltjahr");
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+}
